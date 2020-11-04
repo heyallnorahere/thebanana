431e2f6a0c70999bb59bd7a87acaf7cd70a33d32
Cosmodash <theo.github@bedafamily.com>
2020-11-04 14:16:58 -0800

    add test texture and original models

--- 3dplatformer/src/main.cpp
+++ 3dplatformer/src/main.cpp
@@ -27,22 +27,26 @@ std::string mk64rr_paths(const std::string& path, void*) {
 std::string results_stage_paths(const std::string& path, void*) {
 	return path_helper(path, "Results Stage\\Results Stage\\images\\", "textures\\placeholder\\resultsstage\\");
 }
+std::string test_texture_path(const std::string& path, void*) {
+	return path_helper(path, "3dplatformer\\3dplatformer\\", "");
+}
 void init_game() {
 	thebanana::g_game = new thebanana::game("banana window");
-	thebanana::static_mesh* p = new thebanana::static_mesh("collision");
+	thebanana::static_mesh* p = new thebanana::static_mesh("test_cube");
 	p->get_transform().translate(2.f, 0.75f, 2.f);
 	p->add_tag("test");
-	p->add_component<thebanana::rigidbody>().set_collision_model_name("collision").set_collision_tags({ "ground" }).set_collider_type<thebanana::mlfarrel_model>().set_radius(0.5f).set_origin_offset(glm::vec3(0.f, 0.5f, 0.f));
+	p->add_component<thebanana::rigidbody>().set_collision_model_name("test_cube").set_collision_tags({ "ground" }).set_collider_type<thebanana::mlfarrel_model>().set_radius(0.5f).set_origin_offset(glm::vec3(0.f, 0.5f, 0.f));
 	p->get_component<thebanana::rigidbody>().set_property("mass", 1.5f);
 	thebanana::g_game->get_scene()->add_object(p);
-	p = new thebanana::static_mesh("collision");
+	p = new thebanana::static_mesh("test_cube");
 	p->get_transform().translate(0.f, -1.f, 0.f);
 	p->get_transform().scale(100.f, 1.f, 100.f);
 	p->add_tag("ground");
-	p->add_component<thebanana::rigidbody>().set_collision_model_name("collision");
+	p->add_component<thebanana::rigidbody>().set_collision_model_name("test_cube");
 	thebanana::g_game->get_scene()->add_object(p);
 	thebanana::g_game->add_model_desc({ "player", "models/placeholder/waluigi.fbx", waluigi_paths, thebanana::transform().scale(0.0005f) });
 	thebanana::g_game->add_model_desc({ "collision", "models/placeholder/collision.obj", waluigi_paths, thebanana::transform() });
+	thebanana::g_game->add_model_desc({ "test_cube", "models/cube.obj", test_texture_path, thebanana::transform() });
 	thebanana::g_game->load_models();
 	thebanana::g_game->get_sound_manager()->load_sound("click", "sounds/click.ogg");
 	thebanana::g_game->get_menu_manager()->load_menu(new thebanana::ui::menu("test_menu.json"));

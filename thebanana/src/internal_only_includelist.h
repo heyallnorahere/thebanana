3c0f44c0a0e7f28ad4c3afdf24f4e1cfecb32fa0
Cosmodash <theo.github@bedafamily.com>
2020-11-10 15:24:15 -0800

    fix includelist and compile paths

--- thebanana/src/internal_only_includelist.h
+++ thebanana/src/internal_only_includelist.h
@@ -1,8 +1,8 @@
 #pragma once
 #ifdef BANANA_BUILD
 #include <imgui.h>
-#include <examples/imgui_impl_win32.h>
-#include <examples/imgui_impl_opengl3.h>
+#include <backends/imgui_impl_win32.h>
+#include <backends/imgui_impl_opengl3.h>
 #include <misc/cpp/imgui_stdlib.h>
 #include <steam/steam_api.h>
 #include <yaml-cpp/yaml.h>

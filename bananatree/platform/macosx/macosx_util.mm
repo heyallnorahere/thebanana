52c78205aa6ac81b70e017428fb4c14c068f79a1
Theo Beda <theo.github@bedafamily.com>
2021-03-15 09:52:28 -0700

    add filter to dialogs

--- bananatree/platform/macosx/macosx_util.mm
+++ bananatree/platform/macosx/macosx_util.mm
@@ -12,6 +12,38 @@ bool success, done;
 std::string path;
 namespace bananatree {
     namespace util {
+        static NSArray<NSString*>* parse_filter(const char* filter) {
+            size_t pos = 0;
+            std::vector<std::string> temp;
+            auto find = [&]() -> size_t {
+                size_t offset = pos + 1;
+                std::string str = std::string(filter + offset);
+                for (size_t i = offset; ; i++) {
+                    if (filter[i] == '\0') {
+                        offset = i;
+                        break;
+                    }
+                }
+                if (str.length() == 0) {
+                    offset = std::string::npos;
+                } else {
+                    temp.push_back(str);
+                }
+                return offset;
+            };
+            while ((pos = find()) != std::string::npos) { /* wait*/ }
+            std::vector<NSString*> strings;
+            for (size_t i = 1; i < (temp.size() - (temp.size() % 2)); i += 2) {
+                std::string filetype = temp[i];
+                size_t pos = filetype.find('.');
+                if (pos != std::string::npos) {
+                    filetype = filetype.substr(pos + 1);
+                }
+                strings.push_back(@(filetype.c_str()));
+            }
+            NSArray<NSString*>* array = [NSArray arrayWithObjects:strings.data() count:strings.size()];
+            return array;
+        }
         std::string open_dialog(const char* filter, int* index) {
             thebanana::window window_ = thebanana::g_game->get_window();
             NSWindow* window = thebanana::platform_specific::get_window(window_.m);
@@ -21,6 +53,9 @@ namespace bananatree {
             [panel setResolvesAliases:YES];
             [panel setAllowsMultipleSelection:NO];
             [panel setAccessoryViewDisclosed:NO];
+            NSArray<NSString*>* filetypes = parse_filter(filter);
+            [panel setAllowedFileTypes:filetypes];
+            [filetypes release];
             done = false;
             [panel beginSheetModalForWindow:window completionHandler:^(NSInteger result) {
                 success = (result == NSModalResponseOK);
@@ -49,6 +84,9 @@ namespace bananatree {
             NSWindow* window = thebanana::platform_specific::get_window(window_.m);
             NSSavePanel* panel = [NSSavePanel savePanel];
             [panel setCanCreateDirectories:YES];
+            NSArray<NSString*>* filetypes = parse_filter(filter);
+            [panel setRequiredFileType:[filetypes objectAtIndex:0]];
+            [filetypes release];
             done = false;
             [panel beginSheetModalForWindow:window completionHandler:^(NSInteger result) {
                 success = (result == NSModalResponseOK);

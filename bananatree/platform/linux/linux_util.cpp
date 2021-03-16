46171fa33e8d1383e622960e0bfb41f71e4c8148
Cosmodash <theo.github@bedafamily.com>
2021-03-15 19:51:18 -0700

    add placeholder linux util file

---
+++ bananatree/platform/linux/linux_util.cpp
@@ -0,0 +1,18 @@
+#include <thebanana.h>
+#include "util.h"
+namespace bananatree {
+    namespace util {
+        std::string open_dialog(const char* filter, int* index) {
+            // todo: implement
+            return "";
+        }
+        std::string save_dialog(const char* filter, int* index) {
+            // todo: implement
+            return "";
+        }
+        std::string make_temp_path(const std::string& prefix) {
+            // todo: implement
+            return "";
+        }
+    }
+}
